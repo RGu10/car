9675af0bd9aa9d49365e654470ba390bd9b17224
Ryad <ryad.guerroudj@gmail.com>
2018-03-16 23:54:35 +0100

    change Backgroundcolor in interface builder instead viewController. added new Button

--- car/ViewController.swift
+++ car/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
